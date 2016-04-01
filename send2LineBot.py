af101048d0cee5e7f215e85cc335a15ac07d7f31
zintoo <kzintoo@gmail.com>
2016-04-01 21:11:27 +0700

    Create send2LineBot.py

---
+++ send2LineBot.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import socket
+
+def send2LineBotServer(ip, port, message):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(15)
+        sock.connect((ip, port))
+        sock.send(message)
+        response = sock.recv(1024)
+    except Exception as exp:
+        print("send2LineBotServer()", exp)
+
+send_data="ccadc20b3a4365ee37f6c45150143c203=%s&" % "ข้อความ"
+buffer = str(send_data)#.encode('utf-8')
+send2LineBotServer("192.168.99.100", 54321, buffer)
